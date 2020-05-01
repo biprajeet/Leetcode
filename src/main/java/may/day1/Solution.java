782fc6456f710c2da36ba84d9ac6312209a04039
biprajeet <biprajeet.pal@gmail.com>
2020-05-01 20:13:02 +0530

    May day 1

--- src/main/java/may/day1/Solution.java
+++ src/main/java/may/day1/Solution.java
@@ -1,5 +1,28 @@
 package may.day1;
 
-public class Solution {
+public class Solution extends VersionControl {
 
+	public int firstBadVersion(int n) {
+
+		int start = 1;
+		int end = n;
+		int mid = 0;
+
+		while (start <= end) {
+
+			mid = start + (end - start) / 2;
+
+			if (isBadVersion(mid))
+				end = mid - 1;
+			else
+				start = mid + 1;
+
+		}
+
+		return start;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new Solution().firstBadVersion(5));
+	}
 }
